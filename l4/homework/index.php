e847b2a349b5b143f028fb603c8c0e6842cdafa3
serpstrong <ommuts@gmail.com>
2020-10-12 03:27:06 +0500

    add style

--- l4/homework/index.php
+++ l4/homework/index.php
@@ -9,13 +9,47 @@
 </head>
 <body>
 
-<?php
-$images = scandir("images");
-for ($i = 2; $i < count($images); $i++):?>
-    <a href="server.php?path=images/<?= $images[$i] ?>" target="_blank"><img width="200px" src="images/<?= $images[$i] ?>"></a>
-<?php
-endfor;
-?>
+<style>
+    .container {
+        max-width: 960px;
+        margin: 0 auto;
+    }
 
+    .image__block {
+        display: flex;
+        justify-content: space-between;
+        margin: 30px 0 0 0;
+    }
+
+    .image__block_a {
+        display: inline-block;
+        max-width: 30%;
+    }
+
+    .image__block_a img {
+        display: inline-block;
+        max-width: 100%;
+    }
+</style>
+
+<div class="container">
+    <?php
+    $images = scandir("images");
+    $i = 2;
+    for ($i; $i < count($images); $i++):?>
+
+        <div class="image__block">
+            <?php
+            for ($k = 0; $k < 3; $k++, $i++):?>
+                <a class="image__block_a" href="server.php?path=images/<?= $images[$i] ?>" target="_blank"><img
+                            src="images/<?= $images[$i] ?>"></a>
+            <?php
+            endfor;
+            ?>
+        </div>
+    <?php
+    endfor;
+    ?>
+</div>
 </body>
 </html>
